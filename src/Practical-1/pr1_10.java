1b985a08c37558a39ba40e954b58b30855eca2ce
Rutul Patel <rutulpatel5407@gmail.com>
2025-07-26 18:17:52 +0530

    pr-9,10

---
+++ src/Practical-1/pr1_10.java
@@ -0,0 +1,26 @@
+public class pr1_10 {
+    public static void main(String[] args) {
+
+        double a = Double.parseDouble(args[0]);
+        double b = Double.parseDouble(args[1]);
+        double c = Double.parseDouble(args[2]);
+
+        System.out.println("Quadratic Equation: " + a + "x² + " + b + "x + " + c + " = 0");
+
+        double discriminant = b * b - 4 * a * c;
+
+        if (discriminant > 0) {
+            double root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+            double root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+            System.out.println("Roots are real and distinct.");
+            System.out.println("Root 1 = " + root1);
+            System.out.println("Root 2 = " + root2);
+        } else if (discriminant == 0) {
+            double root = -b / (2 * a);
+            System.out.println("Roots are real and equal.");
+            System.out.println("Root = " + root);
+        } else {
+            System.out.println("Roots are complex and imaginary.");
+        }
+    }
+}
---
+++ src/Practical-1/pr1_9.java
@@ -0,0 +1,10 @@
+public class pr1_9 {
+    public static void main(String[] args) {
+        
+        String name = args[0];
+        int age = Integer.parseInt(args[1]);   
+
+        System.out.println("Hello, "+ name+ "! You are "+ age + " years old.");
+    }
+    
+}
