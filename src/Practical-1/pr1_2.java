28cbf48c2f8a172825e79973ad2fb6d8abf2864f
Rutul Patel <rutulpatel5407@gmail.com>
2025-07-26 18:16:34 +0530

    pr-2&3

---
+++ src/Practical-1/pr1_2.java
@@ -0,0 +1,6 @@
+
+public class pr1_2 {
+    public static void main(String[] args) {
+        System.out.println("Java makes programming fun!");
+    }
+}
---
+++ src/Practical-1/pr1_3.java
@@ -0,0 +1,8 @@
+
+public class pr1_3 {
+    public static void main(String[] args) {
+        System.out.println("############################");
+        System.out.println("# Java Programming Lab #Guni@2005$$");
+        System.out.println("#############################");
+    }
+}
